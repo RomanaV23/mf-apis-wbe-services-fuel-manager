5c5e3057c8acde802ed119f75f9d5c44244bee63
Elisabeteromana <133717204+Elisabeteromana@users.noreply.github.com>
2024-08-28 21:51:20 -0300

    Criação e configuração dos modelos de Veiculo e Consumo.

---
+++ mf-apis-wbe-services-fuel-manager/Models/Consumo.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace mf_apis_wbe_services_fuel_manager.Models
+{
+    [Table("Consumos")]
+    public class Consumo
+    {
+
+        [Key]
+        public int Id { get; set; }
+
+        public string Descricao { get; set; }
+        [Required]
+        public DataType Data { get; set; }
+        [Required]
+        public decimal Valor { get; set; }
+        [Required]
+        public TipoCombustível Tipo { get; set; }
+        [Required]
+        public int VeiculoId { get; set; }
+        public Veiculo Veiculo { get; set; }
+
+
+
+
+
+
+
+    }
+    public enum TipoCombustível
+    {
+        Diesel, 
+        Etanol,
+        Gasolina
+    }
+}
